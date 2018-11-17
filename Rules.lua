d25ebd0be0d9d19ed65d0e50990179e211f478ee
GitLudi <44570998+GitLudi@users.noreply.github.com>
2018-11-17 23:29:36 +0100

    Update and rename Enter.php to Rules.lua

---
+++ Rules.lua
@@ -0,0 +1,3 @@
+1. Whenever you want to edit a file, do it here first, save it and then upload it to the server.
+2. Do not delete files on here.
+3. Whenever you update a file here, explain what you changed with the update.
